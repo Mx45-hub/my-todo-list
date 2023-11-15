5e98e7e1aec1ddd70970313a1bfece0f881934d2
Musa <falcondesignagency@gmail.com>
2023-11-16 00:25:56 +0200

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-8">
+       <h2>About</h2>
+      </main>
+    )
+  }
